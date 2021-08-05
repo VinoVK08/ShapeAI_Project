26509adfc4758cb258c2799a8ef58bff3c4d18b6
VinoVK08 <62206610+VinoVK08@users.noreply.github.com>
2021-08-05 19:02:56 +0100

    Add files via upload

---
+++ src/components/Boot.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Boot() {
+  
+  return (
+    <div className="note">
+      <h1>React Js </h1>
+      <p>This is the framework for frontend</p>
+    </div>
+  );
+}
+export default Boot;
